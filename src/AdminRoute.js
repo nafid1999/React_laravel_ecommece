a09d2983cfee90b69c4460cb3a53b085f634db5b
nafid1999 <73225813+nafid1999@users.noreply.github.com>
2021-08-31 18:24:40 +0200

    add private route

--- src/AdminRoute.js
+++ src/AdminRoute.js
@@ -1,13 +1,49 @@
-import React from 'react'
-import { Redirect, Route } from 'react-router-dom'
+import axios from 'axios';
+import React,{useState,useEffect}from 'react'
+import { Redirect, Route, useHistory } from 'react-router-dom'
 import MainLayout from './layouts/admin/MainLayout'
+import { Spinner } from 'react-bootstrap';
+import swal from 'sweetalert';
 
 const AdminRoute = (props) => {
+
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setloading] = useState(true)
+  const history =useHistory()
+
+  useEffect(() => {
+
+    axios.get("/api/checkAuthentication").then(res=>{
+
+       if(res.data.status===200){
+          console.log("hhhhhhhh")
+          setAuthenticated(true)
+          console.log(loading)
+
+       }
+       setloading(false)
+
+    }).catch(err=>{
+        if(err.response.status===401)
+          swal("Warning",err.response.statusText,"warning")
+       history.push("/")
+    
+    });
+
+  },[])
+
+  if(loading){
+    return (
+      <div className="text-center py-5">
+            <Spinner animation="border" />
+        </div>
+    )
+  }
   console.log(props)
     return (
        <Route 
          path={props.path} exact={true} 
-         render={(props)=>localStorage.getItem('token')?
+         render={(props)=>authenticated?
           <MainLayout {...props}/> :
           <Redirect to="/login" />
         }
