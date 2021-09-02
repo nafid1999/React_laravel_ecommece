a8ca5df0c621c631afc04d8eab968e642b30b29c
nafid1999 <73225813+nafid1999@users.noreply.github.com>
2021-09-02 15:05:04 +0200

    add category api

--- src/components/admin/Categories.js
+++ src/components/admin/Categories.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
+import swal from 'sweetalert'
 
 const Categories = () => {
 
@@ -10,15 +12,15 @@ const Categories = () => {
         metaTitle:'',
         metaKeywords:'',
         metaDesc:'',
-
     })
+    const [errors,setErrors]=useState('');
+
 
     /**
      * 
      * events handler
      */
     
-
     const handlChange=(e)=>{
 
         setcategoryInput({...categoryInput,[e.target.name]:e.target.value})
@@ -26,6 +28,24 @@ const Categories = () => {
     }
     const handlSubmit=(e)=>{
         e.preventDefault()
+
+
+        axios.post("/api/category/create",{...categoryInput}).then(res=>{
+
+            if(res.data.status===200){
+                setErrors('');
+                swal("Success",res.data.message,"success");
+               
+            }else if(res.data.status===403){
+                console.log("error")
+                setErrors(res.data.message.name)
+
+            }
+        }).catch((err)=>{
+            console.log(err)
+         });
+
+
         
 
    }
@@ -33,10 +53,15 @@ const Categories = () => {
     return (
         
         <div className="container-fluid px-4  ">
-            {}
-            <h1 className="mt-4">Add Categorie</h1>
+            {categoryInput.desc}
+            <h1 className="mt-4 ">Add Categorie</h1>
             <div className="col-md-8 mt-4 ">
-                <form onSubmit={handlSubmit}>
+                { 
+                  errors &&
+                  <div className="alert alert-danger">{errors}</div>
+
+                }
+                <form onSubmit={handlSubmit} className="">
                     <ul className="nav nav-pills mb-3 bg-white" id="myTab" role="tablist">
                         <li className="nav-item" role="presentation">
                             <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Add categorie</button>
@@ -46,25 +71,25 @@ const Categories = () => {
                         </li>
                     </ul>
                     <div className="tab-content bg-white px-4 py-2 " id="myTabContent">
-                        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"  >
 
                             <div className="form-group mb-2">
-                                <label forHtml="name" className="mb-3">Name:</label>
-                                <input className="form-control" name="name" type="text" id="name" onChange={handlChange} />
+                                <label htmlFor="name" className="mb-3">Name:</label>
+                                <input className="form-control" name="name" type="text" id="name" onChange={handlChange} value={categoryInput.name}/>
                             </div>
 
                             <div className="form-group mb-2">
-                                <label forHtml="slug" className="mb-3">slug:</label>
-                                <input className="form-control" name="slug" type="text" id="slug" onChange={handlChange} />
+                                <label htmlFor="slug" className="mb-3">slug:</label>
+                                <input className="form-control" name="slug" type="text" id="slug" onChange={handlChange} value={categoryInput.slug} />
                             </div>
 
                             <div className="form-group mb-2">
-                                <label forHtml="desc" className="mb-3">description:</label>
-                                <textarea name="desc" className="form-control" id="desc" onChange={handlChange}></textarea>
+                                <label htmlFor="desc" className="mb-3">description:</label>
+                                <textarea name="desc" className="form-control" id="desc" onChange={handlChange} value={categoryInput.desc}/>
                             </div>
 
-                            <div className="form-group mb-2 ">
-                                <input className="btn btn-primary  " value="submit" />
+                            <div className="form-group mb-2 " style={{overflow:"hidden"}} >
+                                <input  type="submit" className="btn btn-primary float-end " value="add category" />
                             </div>
 
 
@@ -72,22 +97,22 @@ const Categories = () => {
                         <div className="tab-pane fade" id="seo-tag" role="tabpanel" aria-labelledby="seo-tag-tab">
 
                             <div className="form-group mb-2">
-                                <label forHtml="meta-title" className="mb-3">Meta title:</label>
-                                <input className="form-control" name="metaTitle" type="text" id="meta-title" onChange={handlChange} />
+                                <label htmlFor="meta-title" className="mb-3">Meta title:</label>
+                                <input className="form-control" name="metaTitle" type="text" id="meta-title" onChange={handlChange} value={categoryInput.metaTitle} />
                             </div>
 
                             <div className="form-group mb-2">
-                                <label forHtml="meta-keywords" className="mb-3">Meta Keywords:</label>
-                                <input className="form-control" name="metaKeywords" type="text" id="meta-keywords" onChange={handlChange} />
+                                <label htmlFor="meta-keywords" className="mb-3">Meta Keywords:</label>
+                                <input className="form-control" name="metaKeywords" type="text" id="meta-keywords" onChange={handlChange} value={categoryInput.metaKeywords} />
                             </div>
 
                             <div className="form-group mb-2">
-                                <label forHtml="meta-desc" className="mb-3">Meta Description:</label>
-                                <textarea name="metaDesc" className="form-control" id="meta-desc" onChange={handlChange}></textarea>
+                                <label htmlFor="meta-desc" className="mb-3">Meta Description:</label>
+                                <textarea name="metaDesc" className="form-control" id="meta-desc" onChange={handlChange} value={categoryInput.metaDesc}/>
                             </div>
 
-                            <div className="form-group mr-3">
-                                <input className="btn btn-primary " value="submit" />
+                            <div className="form-group mr-3 " style={{overflow:"hidden"}}>
+                                <input type="submit"className="btn btn-primary float-end " value="add category" />
                             </div>
 
                         </div>
